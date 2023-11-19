435dcc646f646a5566aa0c5918512bde0c2cfbec
David Simões <david.simoes@ua.pt>
2023-11-19 21:08:55 +0000

    David/attacks (#2)
    * Crashfix and attack1
    
    * Attack1 done
    
    * Fixed

---
+++ Scripts/attack3.gd
@@ -0,0 +1,21 @@
+extends Area3D
+
+var dir = Vector3()
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	set_as_top_level(true)
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	self.position -= transform.basis.z * 15 * delta
+
+func _on_dmg(body: Node) -> void:
+	if body.is_in_group("Enemy"):
+		body.take_damage(3)
+		# spawn explosion
+		# do slow/dot/more dmg
+		queue_free()
+
+func _on_timeout() -> void:
+	queue_free()
--- Scripts/lich.gd
+++ Scripts/lich.gd
@@ -3,6 +3,8 @@ extends CharacterBody3D
 @onready var navigationAgent : NavigationAgent3D = $NavigationAgent3D
 @onready var camera: Camera3D = get_tree().get_nodes_in_group("Camera")[0]
 @onready var camera_delta: Vector3 = camera.position - self.position
+@onready var projectile_spawner : Node3D = $ProjectileSpawner
+@export var attack1_prefab : PackedScene
 @export var Speed = 6
 
 # Called when the node enters the scene tree for the first time.
@@ -30,9 +32,9 @@ func faceDirection(direction):
 
 func _input(event):
 	if Input.is_action_just_pressed("mouse_move"):
-		mouse_move(event)
+		mouse_move()
 	elif Input.is_action_pressed("attack1"):
-		pass
+		attack1()
 	elif Input.is_action_pressed("attack2"):
 		pass
 	elif Input.is_action_pressed("attack3"):
@@ -42,7 +44,7 @@ func _input(event):
 	elif Input.is_action_pressed("zombie_move_pass"):
 		pass
 
-func mouse_move(event):
+func get_mouse_target_pos():
 	var mousePos = get_viewport().get_mouse_position()
 	var rayLength = 100
 	var from = camera.project_ray_origin(mousePos)
@@ -52,7 +54,21 @@ func mouse_move(event):
 	rayQuery.from = from
 	rayQuery.to = to
 	rayQuery.collide_with_areas = true
-	var result = space.intersect_ray(rayQuery)
+	var result: Dictionary = space.intersect_ray(rayQuery)
+	return result
+
+func mouse_move():
+	var result = get_mouse_target_pos()
 	if not result:
 		return
 	navigationAgent.target_position = result.position
+	
+
+func attack1():
+	var projectile = attack1_prefab.instantiate()
+	var result = get_mouse_target_pos()
+	if not result:
+		return
+	faceDirection(result.position)
+	projectile.rotation_degrees = projectile_spawner.global_transform.basis.get_euler()
+	projectile_spawner.add_child(projectile)
