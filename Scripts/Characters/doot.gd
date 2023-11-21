a7c6300053b3bc79043158548b8372b3b2ba9186
Simão Reis <dracostriker@hotmail.com>
2023-11-21 22:49:04 +0000

    Mobs (#13)
    * knight
    
    * selection minor bug fix
    
    * knight fixed
    
    * extra attack patterns

--- Scripts/Characters/doot.gd
+++ Scripts/Characters/doot.gd
@@ -10,11 +10,12 @@ func update_animation_parameters():
 		anim_tree["parameters/conditions/idle"] = false
 		anim_tree["parameters/conditions/moving"] = true
 	elif state == State.ATK:
-		anim_tree["parameters/conditions/attack1"] = true
+		if atk_pattern == 0:
+			anim_tree["parameters/conditions/attack1"] = true
+		else:
+			anim_tree["parameters/conditions/attack2"] = true
 		anim_tree["parameters/conditions/idle"] = true
 		anim_tree["parameters/conditions/moving"] = false
-	#elif virt_state == State.ATK2:
-	#	anim_tree["parameters/conditions/attack2"] = true
 	elif state == State.DEAD:
 		anim_tree["parameters/conditions/death"] = true
 
