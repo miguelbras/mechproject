f0a272bdaf9d332cdeff797e88d5fb1160a30d49
David Simões <david.simoes@ua.pt>
2023-11-21 19:22:24 +0000

    Crowd control fixes (#10)
    * Bug fix
    
    * Robots stop trying to move if blocked
    
    * Fixed collisions

--- Scripts/Characters/mob.gd
+++ Scripts/Characters/mob.gd
@@ -18,20 +18,46 @@ var stop_dist = 2
 var state = State.IDLE
 var attacking = false
 var enemy_target = null
-var move_target = null
+@onready var move_target = self.position
 var aggressive: bool = true
+
+var last_positions = []
+var last_positions_amount = 20
 
 func _physics_process(delta):
 	if not attacking:
 		if aggressive:
 			follow_enemy()
 		# if no enemy nearby, or if passive, just move to destination
-		elif enemy_target == null:
-			follow_target()
+		if enemy_target == null or not aggressive:
+			follow_target() 
+		check_blocked()
 	update_state()
 	update_animation_parameters()
 	look_at_target()
 	move_and_slide()
+	
+func check_blocked():
+	# return if not trying to move
+	if velocity == Vector3.ZERO:
+		return
+	
+	last_positions.append(self.position)
+	# return if we dont have enough samples
+	if len(last_positions) < last_positions_amount:
+		return
+	
+	while len(last_positions) > last_positions_amount:
+		last_positions.remove_at(0)
+	
+	# check if we've been in the same place for a while
+	var blocked = true
+	for i in range(last_positions_amount):
+		if (last_positions[0] - last_positions[i]).length() > 0.1:
+			blocked = false
+	if blocked:
+		#print("blocked")
+		move_target = self.position
 
 func follow_enemy():
 	enemy_target = Util.get_closest_target(enemy_target, position, cast, "Enemy")
@@ -44,18 +70,24 @@ func follow_enemy():
 		velocity = Vector3.ZERO
 
 func follow_target():
-	if move_target != null:
-		var desired_velocity = (move_target - position) * max_velocity
-		var steering = desired_velocity - velocity
-		velocity = Util.truncate_vector(velocity + steering, max_velocity)
-		velocity.y = 0
-	else:
+	var distance_to_target: Vector3 = move_target - position
+	
+	# dont move if right next to target
+	if distance_to_target.length_squared() < 0.1:
 		velocity = Vector3.ZERO
+		return
+		
+	var desired_velocity = distance_to_target * max_velocity
+	var steering = desired_velocity - velocity
+	velocity = Util.truncate_vector(velocity + steering, max_velocity)
+	velocity.y = 0
 
 func look_at_target():
 	if aggressive and enemy_target != null:
 		self.look_at(enemy_target.position, Vector3.UP, true)
-	elif move_target != null:
+		return
+	# only look if target is far away
+	if (move_target - position).length() > 0.1:
 		self.look_at(move_target, Vector3.UP, true)
 
 func update_state():
@@ -79,10 +111,12 @@ func update_animation_parameters():
 func aggressive_move(position: Vector3):
 	aggressive = true
 	move_target = position
+	last_positions.clear()
 	
 func passive_move(position: Vector3):
 	aggressive = false
 	move_target = position
+	last_positions.clear()
 
 func take_damage(dmg: int):
 	hp -= dmg
