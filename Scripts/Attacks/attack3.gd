5afbb8495e6d5ee7b5c2aa23475bc4f1448ec770
Miguel Brás <miguelbrasneves@gmail.com>
2023-11-22 19:07:02 +0000

    Merged from main into UI

--- Scripts/Attacks/attack3.gd
+++ Scripts/Attacks/attack3.gd
@@ -14,7 +14,9 @@ func _process(delta):
 	self.position -= transform.basis.z * spd * delta
 
 func _on_dmg(body: Node) -> void:
-	if body.is_in_group("Enemy"):
+	if body.is_in_group("Building"):
+		queue_free()
+	elif body.is_in_group("Enemy"):
 		body.take_damage(dmg)
 		# spawn explosion
 		queue_free()
