b16e04f7c8cf2183c887cec622f78a897a824e34
Simão Reis <dracostriker@hotmail.com>
2023-11-30 23:15:36 +0000

    Ultimate fix branch (#58)
    * houses
    
    * wilderness models
    
    * wilderness models (cont.)
    
    * wilderness models (cont. 2)

--- Scripts/Buildings/spawner.gd
+++ Scripts/Buildings/spawner.gd
@@ -5,7 +5,8 @@ class_name Building
 @export var max_civilians: int = 1
 @export var max_knights: int = 0
 @export var max_paladins: int = 0
-@export var civilian_prefab: PackedScene
+@export var female_prefab: PackedScene
+@export var male_prefab: PackedScene
 @export var knight_prefab: PackedScene
 @export var paladin_prefab: PackedScene
 
@@ -31,7 +32,7 @@ func _on_ready():
 		spawn_something_ready(knight_prefab.instantiate())
 		current_knights += 1
 	while current_civilians < max_civilians:
-		spawn_something_ready(civilian_prefab.instantiate())
+		spawn_something_ready(male_prefab.instantiate() if randf() > 0.5 else female_prefab.instantiate())
 		current_civilians += 1
 	set_visuals(false)
 
@@ -43,7 +44,10 @@ func spawn_something():
 		last_spawned_entity = knight_prefab.instantiate()
 		current_knights += 1
 	elif current_civilians < max_civilians:
-		last_spawned_entity = civilian_prefab.instantiate()
+		if randf() > 0.5:
+			last_spawned_entity = male_prefab.instantiate()
+		else:
+			last_spawned_entity = female_prefab.instantiate()
 		current_civilians += 1
 	else:
 		last_spawned_entity = null
