cd385ae8ebbaf6263bf2ef4e633ec82f7b114414
David <david.simoes@ua.pt>
2023-12-01 17:10:16 +0000

    Fortress center

--- Scripts/Attacks/attack1.gd
+++ Scripts/Attacks/attack1.gd
@@ -4,7 +4,7 @@ func _on_dmg(body: Node) -> void:
 	if body.is_in_group("Building"):
 		queue_free()
 	elif body.is_in_group("Enemy"):
-		body.take_damage(100)
+		body.take_damage(strength)
 		# spawn explosion
 		body.set_slow()
 		queue_free()
